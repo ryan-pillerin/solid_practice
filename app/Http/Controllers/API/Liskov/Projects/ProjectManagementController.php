d2b1a5c796e5707cbaed991863c06228863522ee
Ryan Pillerin <ryan.david.pillerin@gmail.com>
2023-07-20 21:13:24 +0800

    Update - Liskov implement using SBH Project Planner.

---
+++ app/Http/Controllers/API/Liskov/Projects/ProjectManagementController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API\Liskov\Projects;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectManagementController extends Controller
+{
+
+}
---
+++ app/Http/Controllers/API/Liskov/Projects/Service/ProjectManagement.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Http\Controllers\API\Liskov\Projects\Service;
+
+class ProjectManagement {
+
+}
